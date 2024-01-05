15fd318565898b9497b5720ec1426a51c5db3f86
Rémi K <remi.kiragossian@gmail.com>
2024-01-05 10:11:00 +0100

    Validations fix attemps

--- custom_components/rika_firenet/__init__.py
+++ custom_components/rika_firenet/__init__.py
@@ -5,13 +5,14 @@ import requests.exceptions
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers import config_validation as cv
 
 from .const import (CONF_DEFAULT_TEMPERATURE, CONF_DEFAULT_SCAN_INTERVAL, CONF_PASSWORD,
                     CONF_USERNAME, DOMAIN, PLATFORMS, STARTUP_MESSAGE)
 from .core import RikaFirenetCoordinator
 
 _LOGGER = logging.getLogger(__name__)
-
+CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
 
 async def async_setup(hass: HomeAssistant, config: dict):
     _LOGGER.info('setup_platform()')
