116415fde62ebcef8ebde9c014f67800e88e1d0e
Rémi K <remi.kiragossian@gmail.com>
2024-01-06 09:54:52 +0100

    remove " " in sensors to fix translation bug. CREATE DUPLICATED ENTITIES, YOU CAN REMOVE/ADD STOVE TO FIX and keep history

--- custom_components/rika_firenet/sensor.py
+++ custom_components/rika_firenet/sensor.py
@@ -12,19 +12,19 @@ from .core import RikaFirenetStove
 _LOGGER = logging.getLogger(__name__)
 
 DEVICE_SENSORS = [
-    "stove consumption",
-    "stove runtime",
-    "stove temperature",
-    "room temperature",
-    "stove thermostat",
-    "stove burning",
-    "stove status",
-    "pellets before service",
-    "fan velocity",
-    "diag motor",
-    "number fail",
-    "main state",
-    "sub state",
+    "stove_consumption",
+    "stove_runtime",
+    "stove_temperature",
+    "room_temperature",
+    "stove_thermostat",
+    "stove_burning",
+    "stove_status",
+    "pellets_before_service",
+    "fan_velocity",
+    "diag_motor",
+    "number_fail",
+    "main_state",
+    "sub_state",
     "statusError",
     "statusSubError"
 ]
@@ -56,31 +56,31 @@ class RikaFirenetStoveSensor(RikaFirenetEntity):
 
     @property
     def state(self):
-        if self._sensor == "stove consumption":
+        if self._sensor == "stove_consumption":
             return self._stove.get_stove_consumption()
-        elif self._sensor == "stove runtime":
+        elif self._sensor == "stove_runtime":
             return self._stove.get_stove_runtime()
-        elif self._sensor == "stove temperature":
+        elif self._sensor == "stove_temperature":
             return self._stove.get_stove_temperature()
-        elif self._sensor == "room temperature":
+        elif self._sensor == "room_temperature":
             return self._stove.get_room_temperature()
-        elif self._sensor == "stove thermostat":
+        elif self._sensor == "stove_thermostat":
             return self._stove.get_room_thermostat()
-        elif self._sensor == "stove burning":
+        elif self._sensor == "stove_burning":
             return self._stove.is_stove_burning()
-        elif self._sensor == "stove status":
+        elif self._sensor == "stove_status":
             return self._stove.get_status_text()
-        elif self._sensor == "pellets before service":
+        elif self._sensor == "pellets_before_service":
             return self._stove.get_pellets_before_service()
-        elif self._sensor == "diag motor":
+        elif self._sensor == "diag_motor":
             return self._stove.get_diag_motor()
-        elif self._sensor == "fan velocity":
+        elif self._sensor == "fan_velocity":
             return self._stove.get_fan_velocity()
-        elif self._sensor == "number fail":
+        elif self._sensor == "number_fail":
             return self._stove.get_number_fail()
-        elif self._sensor == "main state":
+        elif self._sensor == "main_state":
             return self._stove.get_main_state()
-        elif self._sensor == "sub state":
+        elif self._sensor == "sub_state":
             return self._stove.get_sub_state()
         elif self._sensor == "statusError" :
             return self._stove.get_status_error()
@@ -92,9 +92,9 @@ class RikaFirenetStoveSensor(RikaFirenetEntity):
     def unit_of_measurement(self):
         if "temperature" in self._sensor or "thermostat" in self._sensor:
             return UnitOfTemperature.CELSIUS
-        elif self._sensor == "stove consumption" or self._sensor == "pellets before service":
+        elif self._sensor == "stove_consumption" or self._sensor == "pellets_before_service":
             return UnitOfMass.KILOGRAMS
-        elif self._sensor == "stove runtime":
+        elif self._sensor == "stove_runtime":
             return UnitOfTime.HOURS
 
 
@@ -102,39 +102,39 @@ class RikaFirenetStoveSensor(RikaFirenetEntity):
     def icon(self):
         if "temperature" in self._sensor or "thermostat" in self._sensor:
             return "mdi:thermometer"
-        elif self._sensor == "stove consumption" or self._sensor == "pellets before service":
+        elif self._sensor == "stove_consumption" or self._sensor == "pellets_before_service":
             return "mdi:weight-kilogram"
-        elif self._sensor == "stove runtime":
+        elif self._sensor == "stove_runtime":
             return "mdi:timer-outline"
-        elif self._sensor == "stove burning":
+        elif self._sensor == "stove_burning":
             return "mdi:fire"
-        elif self._sensor == "stove status" or self._sensor == "number fail" or self._sensor == "main state" or self._sensor == "sub state":
+        elif self._sensor == "stove_status" or self._sensor == "number_fail" or self._sensor == "main_state" or self._sensor == "sub_state":
             return "mdi:information-outline"
-        elif self._sensor == "diag motor" or self._sensor == "fan velocity":
+        elif self._sensor == "diag_motor" or self._sensor == "fan_velocity":
             return "mdi:speedometer"
 
 
     @property
     def entity_category(self):
-        if self._sensor == "stove consumption":
+        if self._sensor == "stove_consumption":
             return EntityCategory.DIAGNOSTIC
-        elif self._sensor == "stove runtime":
+        elif self._sensor == "stove_runtime":
             return EntityCategory.DIAGNOSTIC
-        elif self._sensor == "stove temperature":
+        elif self._sensor == "stove_temperature":
             return EntityCategory.DIAGNOSTIC
-        elif self._sensor == "stove burning":
+        elif self._sensor == "stove_burning":
             return EntityCategory.DIAGNOSTIC
-        elif self._sensor == "pellets before service":
+        elif self._sensor == "pellets_before_service":
             return EntityCategory.DIAGNOSTIC
-        elif self._sensor == "diag motor":
+        elif self._sensor == "diag_motor":
             return EntityCategory.DIAGNOSTIC 
-        elif self._sensor == "fan velocity":
+        elif self._sensor == "fan_velocity":
             return EntityCategory.DIAGNOSTIC 
-        elif self._sensor == "number fail":
+        elif self._sensor == "number_fail":
             return EntityCategory.DIAGNOSTIC 
-        elif self._sensor == "main state":
+        elif self._sensor == "main_state":
             return EntityCategory.DIAGNOSTIC 
-        elif self._sensor == "sub state":
+        elif self._sensor == "sub_state":
             return EntityCategory.DIAGNOSTIC 
         elif self._sensor == "statusError" :
             return EntityCategory.DIAGNOSTIC 
